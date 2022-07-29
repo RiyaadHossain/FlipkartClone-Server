1f73926a7a3d593fe20cb9201c5a45044c730484
RiyaadHossain <riyadhossain.dev@gamil.com>
2022-07-29 09:42:20 +0600

    unique product replace the whole products - problem solved

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,31 +1,37 @@
 const Cart = require("../models/cartModel");
 
 // Add To Cart_________________________
-exports.addToCart = (req, res) => {
+exports.addToCart = async (req, res) => {
   const { cartItems } = req.body;
   const user = req.user._id;
 
   try {
-
-    Cart.findOne({ user }).exec((err, cart) => {
+    Cart.findOne({ user }).exec(async (err, cart) => {
       if (err)
-        return res.status(400).json({ error: "Internal Server Error Occured..!" });
+        return res
+          .status(400)
+          .json({ error: "Internal Server Error Occured..!" });
 
       // * The Cart is exist_____
       if (cart) {
         const item = cart.cartItems.find((i) => i.product == cartItems.product);
+
         if (item) {
           // * Same Product_____
           Cart.findOneAndUpdate(
-            { user: user, "cartItems.product": cartItems.product },
+            { user, "cartItems.product": cartItems.product },
             {
               $set: {
-                ...cartItems,
-                quantity: item.quantity + cartItems.quantity,
+                "cartItems.$": {
+                  ...cartItems,
+                  quantity: Number(item.quantity) + Number(cartItems.quantity),
+                },
               },
+            },
+            {
+              new: true,
             }
           ).exec((err, data) => {
-
             if (err) {
               return res
                 .status(400)
@@ -34,43 +40,51 @@ exports.addToCart = (req, res) => {
             if (data) {
               if (data)
                 return res.status(200).json({
-                  message: "Product added to cart Successfully. - Same Product",
+                  // message: "Product added to cart Successfully. - Same Product",
+                  data,
                 });
             }
           });
-          
         } else {
           // * Unique Product_____
-          console.log("Unique Product");
-          Cart.findOneAndUpdate(user, {
-            $push: { cartItems: cartItems },
-          }).exec((err, data) => {
+          Cart.findOneAndUpdate(
+            user,
+            {
+              $push: { cartItems: cartItems },
+            },
+            {
+              new: true,
+            }
+          ).exec((err, data) => {
             if (err) {
-              return res.status(400).json({ error: "Internal Server Error Occured..!" });
+              return res
+                .status(400)
+                .json({ error: "Internal Server Error Occured..!" });
             }
             if (data) {
               if (data)
                 return res.status(200).json({
-                  message:
-                    "Product added to cart Successfully. - Unique Product",
+                  // message:"Product added to cart Successfully. - Unique Product",
+                  data,
                 });
             }
           });
         }
       } else {
-
         // * The Cart is not exist_____
         const newCart = Cart({ user, cartItems });
         newCart.save((err, data) => {
           if (err)
-            return res.status(400).json({ error: "Internal Server Error Occured..!" });
+            return res
+              .status(400)
+              .json({ error: "Internal Server Error Occured..!" });
 
           if (data)
             return res.status(200).json({
-              message: "Product added to cart Successfully. - Unique User",
+              // message: "Product added to cart Successfully. - Unique User",
+              data,
             });
         });
-
       }
     });
   } catch (error) {
